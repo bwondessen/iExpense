85d2e8a11e5a5453ae384c24963f01748d3430ee
Bruke Wondessen <bwondessen23@gmail.com>
2022-03-09 15:35:14 -0600

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Bruke on 3/9/22.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
